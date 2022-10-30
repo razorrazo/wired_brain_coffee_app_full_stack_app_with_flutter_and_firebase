ae570f0e49b449b6fb6542dbe12c3c4f94fb24a9
marcelo <marcelo94@gmail.com>
2022-10-30 00:06:24 -0400

    App con base en flutter y conectado con firebase con la opcion de analytics habilitada y auth igual

---
+++ android/app/src/main/kotlin/com/marceloadrian/wired_brain_coffe_flutter_application/wired_brain_coffe_flutter_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.marceloadrian.wired_brain_coffe_flutter_application.wired_brain_coffe_flutter_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
